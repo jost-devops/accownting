c4b0d482efb98a21405cda7d8f912236288dae10
Daniel Jost <daniel@jost-devops.de>
2018-12-03 18:26:42 +0100

    remove non-working cron stuff

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,10 +6,4 @@ wait-for-it -t 60 -h db -p 3306 -s -- php /var/www/bin/console doctrine:migratio
 
 echo "$(date) Doctrine Migrations executed ..."
 
-# touch cron stuff due to debian stretch security policy --> https://unix.stackexchange.com/questions/453006/getting-cron-to-work-on-docker
-touch /etc/crontab /etc/cron.*/*
-
-# start cron scheduler (in background automatically)
-service cron start
-
 apache2-foreground
