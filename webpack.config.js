9935d378c3f92cd2aa5c7cb26e5c0cc069eabecb
Daniel Jost <daniel.jost@jost-devops.de>
2021-06-14 22:51:45 +0200

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,6 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
 
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
@@ -65,10 +63,10 @@ Encore
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+    .enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
