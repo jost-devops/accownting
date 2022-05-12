b5c1b43d396d48870ed8f662c263707d5c13faeb
Daniel Jost <daniel.jost@jost-devops.de>
2022-05-12 11:05:08 +0200

    force workflow run

--- src/Controller/VatController.php
+++ src/Controller/VatController.php
@@ -29,7 +29,7 @@ class VatController extends AbstractController
             ->findAll();
 
         $begin = (new \DateTime())
-            ->sub(new \DateInterval('P25M'))
+            ->sub(new \DateInterval('P24M'))
             ->modify('first day of this month')
             ->setTime(0, 0, 0);
         $end = (new \DateTime());
